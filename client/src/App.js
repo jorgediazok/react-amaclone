f50a6a37bc7fb4b94b292007d54d341636541edb
Jorge <jorgediazok@gmail.com>
2020-09-16 15:33:27 -0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Amaclone</header>
+      <aside>Shopping Categories</aside>
+      <main>
+        <ul>
+          <li>Product 1</li>
+          <li>Product 2</li>
+          <li>Product 3</li>
+          <li>Product 4</li>
+          <li>Product 5</li>
+        </ul>
+      </main>
+      <footer>All Rights Reserved</footer>
+    </div>
+  );
+}
+
+export default App;
