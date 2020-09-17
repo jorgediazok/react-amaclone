bfc2cb9e695c23328ad57ec133592ab448387c42
Jorge <jorgediazok@gmail.com>
2020-09-16 23:13:07 -0300

    adding structure

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header>Amaclone</header>
-      <aside>Shopping Categories</aside>
-      <main>
+    <div className="grid-container">
+      <header className="header">
+        <div className="logo">
+          <a href="/">Amaclone</a>
+        </div>
+        <div className="header-links">
+          <a href="/cart">Cart</a>
+          <a href="/login">Login</a>
+        </div>
+      </header>
+      <main className="main">
         <ul>
           <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
-          <li>Product 4</li>
-          <li>Product 5</li>
         </ul>
       </main>
-      <footer>All Rights Reserved</footer>
+      <footer className="footer">All Rights Reserved</footer>
     </div>
   );
 }
