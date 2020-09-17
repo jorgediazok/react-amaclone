3efbb3cd5a2e4b6c0fca7b3e3b7bfd77c0bd5d9a
Jorge <jorgediazok@gmail.com>
2020-09-17 00:59:44 -0300

    more styles

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,12 @@ function App() {
     <div className="grid-container">
       <header className="header">
         <div className="logo">
+          <button>&#9776;</button>
           <a href="/">Amaclone</a>
         </div>
         <div className="header-links">
-          <a href="/cart">Cart</a>
-          <a href="/login">Login</a>
+          <a href="/cart">CART</a>
+          <a href="/login">LOGIN</a>
         </div>
       </header>
       <main className="main">
@@ -28,6 +29,37 @@ function App() {
                 </div>
                 <div className="product-brand">Hugo Boss</div>
                 <div className="product-price">$59.99</div>
+                <div className="product-rating">4.5 Stars (10 reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img
+                  className="product-image"
+                  src="/images/image1.jpg"
+                  alt="shirt"
+                />
+                <div className="product-name">
+                  <a href="/product">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Hugo Boss</div>
+                <div className="product-price">$59.99</div>
+                <div className="product-rating">4.5 Stars (10 reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img
+                  className="product-image"
+                  src="/images/image6.jpg"
+                  alt="shirt"
+                />
+                <div className="product-name">
+                  <a href="/product">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Hugo Boss</div>
+                <div className="product-price">$59.99</div>
+                <div className="product-rating">4.5 Stars (10 reviews)</div>
               </div>
             </li>
           </ul>
