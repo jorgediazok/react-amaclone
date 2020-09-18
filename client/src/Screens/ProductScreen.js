acf94e807fe23afccdb75f81061da0e4e8f29ec4
Jorge <jorgediazok@gmail.com>
2020-09-18 17:44:39 -0300

    product section done

--- client/src/Screens/ProductScreen.js
+++ client/src/Screens/ProductScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import data from '../data';
 
 function ProductScreen(props) {
@@ -7,7 +8,49 @@ function ProductScreen(props) {
 
   return (
     <div>
-      <h1>{product.name}</h1>
+      <div className="back-to-result">
+        <Link to="/">Back to result</Link>
+      </div>
+      <div className="details">
+        <div className="details-image">
+          <img src={product.image} alt="product" />
+        </div>
+        <div className="details-info">
+          <ul>
+            <li>
+              <h4>{product.name}</h4>
+            </li>
+            <li>
+              {product.rating} Stars ({product.numReviews} Reviews)
+            </li>
+            <li>
+              Price: <b>{product.price}</b>
+            </li>
+            Description:
+            <div>
+              <li>{product.description}</li>
+            </div>
+          </ul>
+        </div>
+        <div className="details-action">
+          <ul>
+            <li>Price:{product.price}</li>
+            <li>Status: {product.status}</li>
+            <li>
+              Qty:
+              <select>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </select>
+            </li>
+            <li>
+              <button className="button">ADD TO CART</button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
